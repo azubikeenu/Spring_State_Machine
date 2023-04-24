9689d2c8366c925e53d6410c7a897fa8cfa585fb
Enu Azubike <enuazubike88@gmail.com>
2023-04-24 13:15:54 +0100

    initial commit

---
+++ src/test/java/com/azubike/ellipsis/spring_state_machine/SpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azubike.ellipsis.spring_state_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
